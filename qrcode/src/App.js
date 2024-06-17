a592c1e64dd9cb0bec4e8e6497f54745a4c2e8fb
stsoy01 <tsoy@perco.ru>
2024-06-17 15:33:28 +0300

    init

---
+++ qrcode/src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import QrCode from "./components/qrcode/QR";
+
+function App() {
+    return (
+        <QrCode></QrCode>
+    );
+}
+
+export default App;
---
+++ qrcode/src/components/qrcode/QR.js
@@ -0,0 +1,5 @@
+export default function QrCode() {
+    return (
+        <>QR</>
+    )
+}
