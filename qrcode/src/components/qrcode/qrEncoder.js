3860f4cb639e25e2bbdc6e7f18a20a606c8d67e1
Sergey Tsoy <74413331+stsoy01@users.noreply.github.com>
2024-12-28 11:13:26 +0300

    Update qrEncoder.js

--- qrcode/src/components/qrcode/qrEncoder.js
+++ qrcode/src/components/qrcode/qrEncoder.js
@@ -3,33 +3,32 @@ import {errorCorrectionCodeWords} from "./errorCorrectionVersion";
 
 export default class Encoder extends EncodeData {
 
-  errorCorrectionObject = {}
-    counter = 0;
+  errorCorrectionObject = {};
+  counter = 0;
+  stringArray = [];
 
   alphanumericEncoding(mode = 'alphaNumeric', str) {
     const string = str.toUpperCase().split('');
-    let stringArray = [];
     let counter = 0;
 
     for (let i = 0; i < string.length; i++) {
-
       if (i % 2 === 0) {
         counter = this.alphanumericTable[string[i]] * 45;
         counter += this.alphanumericTable[string[i + 1]]
 
         if (counter) {
-          stringArray.push(this.decimalToBinary(counter))
+          this.stringArray.push(this.decimalToBinary(counter))
         } else {
           counter = this.alphanumericTable[string[i]];
-          stringArray.push(this.decimalToBinary(counter))
+          this.stringArray.push(this.decimalToBinary(counter))
         }
       }
-
+      
       counter = 0;
     }
 
     return {
-      encodedData: stringArray,
+      encodedData: this.stringArray,
       modeIndicator: {
         name: mode,
         data: this.modeIndicator[mode]
@@ -41,17 +40,17 @@ export default class Encoder extends EncodeData {
     };
   }
 
-
   decimalToBinary(number) {
     let binaryNumber = '';
+  
     while (number / 2 !== 0) {
       binaryNumber += number % 2;
       number = Math.floor(number / 2);
     }
+    
     binaryNumber = binaryNumber.split('').reverse().join('');
+    
     if (binaryNumber.length >= 11) return binaryNumber;
-
-
         while (binaryNumber.length !== 11) {
             binaryNumber = '0' + binaryNumber;
         }
@@ -81,6 +80,7 @@ export default class Encoder extends EncodeData {
     complete8BitCapacity(modeIndicator, characterCountIndicator, encodedData, dataBitCapacity, terminator = '0000') {
         let _8bitString = `${modeIndicator}${characterCountIndicator}${encodedData.join('')}${terminator}`
         _8bitString = _8bitString.match(/.{1,8}/g).map(el => el.length !== 8 ? el.padEnd(8, '0') : el)
+       
         return _8bitString.join('').padEnd(dataBitCapacity, '1110110000010001')
     }
 
